713c307e8a46784f46101a6cfa9e0526a84f4994
Soulard Thibaut <160162383+SoulardThibaut@users.noreply.github.com>
2024-12-19 16:28:15 +0100

    Update 0.3.RunStepTuning.py

--- 0.3.RunStepTuning.py
+++ 0.3.RunStepTuning.py
@@ -11,22 +11,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--relation',
-        default = 'P31',
-        type=str
-    )
-    parser.add_argument(
         '-type',
-        type=str
-    )
-    parser.add_argument(
-        '--hdt_querier',
-        default = './../../../../../sharewithserver/QueryHDT/SparqlHomemade2.jar',
-        type=str
-    )
-    parser.add_argument(
-        '--hdt_file',
-        default = './../../../../../Graphs_HDT/Wikidata/Wikidata_final.hdt',
         type=str
     )
     args = parser.parse_args()
@@ -34,25 +19,6 @@ if __name__ == '__main__':
     type_ent = args.type
     hdtsparql_file = args.hdt_querier
     hdt_file = args.hdt_file
-    
-                    ##### DATA GENERATION
-    print("Data Generation")
-
-    os.chdir("./1.DataGeneration")
-
-    cmd = f"python3 ./1.QueryGenerator.py {relation} {type_ent}"
-    check_call(cmd, shell=True, stdout=DEVNULL, stderr=STDOUT)
-
-    cmd = f"python3 ./2.QueryLauncher.py {type_ent} {hdtsparql_file} {hdt_file}"
-    check_call(cmd, shell=True, stdout=DEVNULL, stderr=STDOUT)
-    
-    cmd = f"python3 ./3.GenerateData.py {type_ent}"
-    check_call(cmd, shell=True, stdout=DEVNULL, stderr=STDOUT)
-
-    cmd = f"python3 ./4.SpliterRel.py {type_ent}"
-    check_call(cmd, shell=True, stdout=DEVNULL, stderr=STDOUT)
-
-    os.chdir("./../")
 
     path = f"./0.Data/{type_ent}/"
     gt_pd = pd.read_csv(f"{path}valid.gt", names=["GT_value"])
